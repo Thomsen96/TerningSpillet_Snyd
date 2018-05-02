d4c8e3facd4c0177c7faebc9f3aa7bb36a65c005
ChristianMark <christian.s.mark@hotmail.com>
2018-05-02 16:57:04 +0200

    Kommentering og formatering

--- src/Server/ServerLogik.java
+++ src/Server/ServerLogik.java
@@ -12,35 +12,35 @@ import java.util.Scanner;
  * @author john
  */
 public class ServerLogik {
+
     private static Spil spilLogik;
     private static ServerFunk serverLogik;
     private static Scanner tastatur = new Scanner(System.in);
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Indtast porten du vil åbne på:");
-        
+
         int port = læsINTtastatur();
         System.out.println("Indtast antallet af spillere:");
         int antalSpillere = læsINTtastatur();
         System.out.println("Indtast antallet af terninger:");
         int antalTerninger = læsINTtastatur();
         //int port = 8998;
-//        int antalSpillere = 2;
-//        int antalTerninger = 6;
-        
-        
+        //int antalSpillere = 2;
+        //int antalTerninger = 6;
+
         serverLogik = new ServerFunk(port);
         spilLogik = new Spil(antalSpillere, antalTerninger);
-        
+
         serverLogik.modtagForbindelse(antalSpillere);
         serverLogik.initierSpil();
-        
-        
-        while(true){
-           switch(spilLogik.getSpil_status()) {
+
+        while (true) {
+            switch (spilLogik.getSpil_status()) {
                 case "start":
                     spilLogik.printTerninger(0); // Print terninger
                     serverLogik.initierRunde(Spil.liste_af_raflebaeger, spilLogik.antal_terninger_ialt()); //skriv til spillere runden begynder
@@ -58,44 +58,44 @@ public class ServerLogik {
                     serverLogik.initierRunde(Spil.liste_af_raflebaeger, spilLogik.antal_terninger_ialt()); //skriv til spillere runden begynder
                     spilLogik.start_rounde(); // Start en ny runde
                     printstats(); // Viser alle kombi
-                    break; 
+                    break;
                 case "spil_slut":
                     serverLogik.spilSlut(spilLogik.getTaber());
                     System.out.println("Spillet er færdigt!");
                     return; // Afslut spil                
                 default:
                     System.out.println("Fejl, ugyldig tilstand");
-                    return;    
-            }    
+                    return;
+            }
         }
     }
-    
-    
-    
-    
+
     /**
      * Behandler en komando fra spille til spillogikken
-     * @param streng 
+     *
+     * @param streng
      */
     private static void læsCommandov3(String streng) {
         int turFørSkift = spilLogik.getHvis_tur();
-        if (streng == null) return;     //ikke crashe når spillere lukker forbindelsen
-        
-        
-        if(streng.startsWith("Guess(") && streng.endsWith(")")){
+        if (streng == null) {
+            return;     //ikke crashe når spillere lukker forbindelsen
+        }
+
+        if (streng.startsWith("Guess(") && streng.endsWith(")")) {
             streng = streng.substring(6);
             int antal = 0;
             int værdi = 0;
-            
+
             try {
-                String antal_string = streng.substring(0,streng.indexOf(","));
+                String antal_string = streng.substring(0, streng.indexOf(","));
 
                 for (int i = 0; i < antal_string.length(); i++) {
-                    if(antal_string.charAt(i) > 57 || antal_string.charAt(i) < 48){
+                    if (antal_string.charAt(i) > 57 || antal_string.charAt(i) < 48) {
                         antal = -1;
                         i = antal_string.length();
-                    }else
-                        antal += ((int) antal_string.charAt(i)-48)* (int) Math.pow(10, antal_string.length()-1-i);
+                    } else {
+                        antal += ((int) antal_string.charAt(i) - 48) * (int) Math.pow(10, antal_string.length() - 1 - i);
+                    }
                 }
             } catch (Exception e) {
                 e.printStackTrace();
@@ -103,71 +103,73 @@ public class ServerLogik {
             }
 
             try {
-                String værdi_string = streng.substring(streng.indexOf(",")+1,streng.indexOf(")"));
+                String værdi_string = streng.substring(streng.indexOf(",") + 1, streng.indexOf(")"));
                 for (int i = 0; i < værdi_string.length(); i++) {
-                    if(værdi_string.charAt(i) > 57 || værdi_string.charAt(i) < 48){
+                    if (værdi_string.charAt(i) > 57 || værdi_string.charAt(i) < 48) {
                         værdi = -1;
                         i = værdi_string.length();
-                    }else
-                        værdi += ((int) værdi_string.charAt(i)-48)* (int) Math.pow(10, værdi_string.length()-1-i);
-                }                                
+                    } else {
+                        værdi += ((int) værdi_string.charAt(i) - 48) * (int) Math.pow(10, værdi_string.length() - 1 - i);
+                    }
+                }
             } catch (Exception e) {
                 e.printStackTrace();
                 værdi = 0;
             }
 
-            spilLogik.gæt(værdi,antal);
-            
+            spilLogik.gæt(værdi, antal);
+
             //Serveren sender beskeder ud om tilstanden af spillet
-            if (spilLogik.getHvis_tur() != turFørSkift){
+            if (spilLogik.getHvis_tur() != turFørSkift) {
                 serverLogik.spillerGættede(turFørSkift, antal, værdi);
             } else {
                 serverLogik.spillerGættedeFejl(turFørSkift, antal, værdi);
             }
             //end if - gæt
-        }else if(streng.matches("Liar!")){
+        } else if (streng.matches("Liar!")) {
             spilLogik.løgner();
-            
+
             //Serverlogikken sender beskeder ud om tilstanden.
             try {
-                
-            if(spilLogik.forrige_gæt != null){ 
-                if (spilLogik.getTaber() != turFørSkift ){
-                    serverLogik.spillerKaldteSnyd(turFørSkift, true, spilLogik.forrige_gæt, stringStats());
-                } else { 
-                    serverLogik.spillerKaldteSnyd(turFørSkift, false, spilLogik.forrige_gæt, stringStats());
+
+                if (spilLogik.forrige_gæt != null) {
+                    if (spilLogik.getTaber() != turFørSkift) {
+                        serverLogik.spillerKaldteSnyd(turFørSkift, true, spilLogik.forrige_gæt, stringStats());
+                    } else {
+                        serverLogik.spillerKaldteSnyd(turFørSkift, false, spilLogik.forrige_gæt, stringStats());
+                    }
+                } else {
+                    serverLogik.spillerKaldteSnydUgyldigt(turFørSkift);
                 }
-            } else {
-                serverLogik.spillerKaldteSnydUgyldigt(turFørSkift);
-            }
-            }catch (Exception e) {
+            } catch (Exception e) {
                 e.printStackTrace();
-                System.err.println("Streng er: "+streng);
+                System.err.println("Streng er: " + streng);
             }
-        }else{
-            System.out.println("Ugyldig kommando!: "+streng);
-            
+        } else {
+            System.out.println("Ugyldig kommando!: " + streng);
+
             //server funk
             serverLogik.spillerUgyldigKomando(turFørSkift);
         }
 
     }
-    
+
     private static void printstats() {
         for (int i = 1; i < 7; i++) {
             System.out.println("   Terninger    Komb");
-            System.out.println(i+"'er: "+spilLogik.getAntalØjne(i)+" \t "+spilLogik.getKombinationer(i));
-        }      
+            System.out.println(i + "'er: " + spilLogik.getAntalØjne(i) + " \t " + spilLogik.getKombinationer(i));
+        }
     }
-    
+
     private static String stringStats() {
         String streng = new String();
         streng += "Terninger\t1'ere  2'ere  3'ere  4'ere  5'ere  6'ere;\t  ";
         for (int i = 1; i < 7; i++) {
-            streng += ""+spilLogik.getAntalØjne(i)+"         ";
+            streng += "" + spilLogik.getAntalØjne(i) + "         ";
         }//end for
         return streng;
     }
+
     public static int læsINTtastatur() {
         int læstint = 0;
 
