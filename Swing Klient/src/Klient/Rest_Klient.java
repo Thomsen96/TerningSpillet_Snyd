112bb8ec5b18aed1ad1cfca0162359245da3e76c
Jonas Thomsen <jonastoftethomsen@gmail.com>
2019-03-12 12:18:02 +0100

    Rest interface startet

---
+++ Swing Klient/src/Klient/Rest_Klient.java
@@ -0,0 +1,18 @@
+
+package Klient;
+
+import java.util.ArrayList;
+
+public class Rest_Klient {
+    SpilData createGame(String user, String Pass, int tern, int spillere) {
+        return null;
+    }
+    
+    ArrayList<SpilData> getGames() {
+        return null;
+    }
+    
+    
+    
+    
+}
