55dd51cb6bb10dc5aa9126e7debe98ad608e1fd2
john <36444142+unizippro@users.noreply.github.com>
2019-03-12 13:33:02 +0100

    Get games retunerer "brugernavn"

--- REST_Terning_server/src/java/helloworld/RESTServerResource.java
+++ REST_Terning_server/src/java/helloworld/RESTServerResource.java
@@ -23,13 +23,13 @@
  * License Header, with the fields enclosed by brackets [] replaced by
  * your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
- * 
+ *
  * Contributor(s):
- * 
+ *
  * The Original Software is NetBeans. The Initial Developer of the Original
  * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
  * Microsystems, Inc. All Rights Reserved.
- * 
+ *
  * If you wish your version of this file to be governed by only the CDDL
  * or only the GPL Version 2, indicate your decision by adding
  * "[Contributor] elects to include this software in this distribution
@@ -41,7 +41,6 @@
  * Version 2 license, then the option applies only if the new code is
  * made subject to such option by the copyright holder.
  */
-
 package helloworld;
 
 import brugerautorisation.transport.soap.Bruger;
@@ -66,27 +65,25 @@ import org.json.JSONObject;
  *
  * @author mkuchtiak
  */
-
 @Stateless
 @Path("")
 public class RESTServerResource {
 
     @EJB
     private NameStorageBean nameStorage = new NameStorageBean();
-    
-    
-    
+
     //private HashMap<String, String> tokenMap = new HashMap<String, String>(); //Usernames mapped to tokens
     /**
      * Retrieves representation of an instance of helloworld.HelloWorldResource
+     *
      * @return an instance of java.lang.String
      */
     @GET
     @Produces("text/html")
     public String getGreeting() {
-        return "<html><body><h1>Hello "+nameStorage.getName()+"!</h1></body></html>";
+        return "<html><body><h1>Hello " + nameStorage.getName() + "!</h1></body></html>";
     }
-    
+
     @Path("games")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
@@ -97,47 +94,44 @@ public class RESTServerResource {
         game.put("spillere", 22022);
         game.put("terninger", 22);
         game.put("token", 44);
+        game.put("brugernavn", "madas");
         JSONObject game2 = new JSONObject();
         game2.put("spillere", 3300);
         game2.put("terninger", 6);
         game2.put("token", 5);
-        
-        
-        
+        game2.put("brugernavn", "casam");
+
         games.put(game);
         games.put(game2);
         return Response.ok(games.toString(), MediaType.APPLICATION_JSON).build();
     }
-    
+
     @Path("games")
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response postGames(String content){
+    public Response postGames(String content) {
         JSONObject recived = new JSONObject(content);
         JSONObject response = new JSONObject();
         String token = recived.getString("token");
         String username = recived.getString("username");
-        
+
         //Authenticate
-        if(Storage.tokenMap.get(username).equals(token)){
+        if (Storage.tokenMap.get(username).equals(token)) {
             //TODO: Code here to gennerate game over RMI
-            
-            
+
             response.put("port", 2); //return port
             response.put("spillere", recived.optString("spillere")); //return spillere
             response.put("terninger", recived.optString("terninger")); //return spillere
             return Response.ok(response.toString(), MediaType.APPLICATION_JSON).build();
-        }else {
+        } else {
             //not valid user/token
-            System.out.println("User failed to authenticate with username" + username + " and token: " +token);
+            System.out.println("User failed to authenticate with username" + username + " and token: " + token);
             response.put("url", "https://http.cat/401");
             return Response.status(Response.Status.UNAUTHORIZED).entity(response.toString()).build();
         }
-        
-        
+
     }
-    
-    
+
     @Path("login")
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
@@ -145,65 +139,53 @@ public class RESTServerResource {
         //TODO return proper representation object
         JSONObject login = new JSONObject(content);
         JSONObject response = new JSONObject();
-        
+
         Boolean loginSucess = Boolean.FALSE;
-        
-        
+
         String username = login.optString("username");
         String password = login.getString("password");
-        
-        System.out.println("Recived login: " + username + "@" +password);
-        
-        
+
+        System.out.println("Recived login: " + username + "@" + password);
+
         //Contact brugerautorisationsmodul
-        try{
+        try {
             Bruger user = hentBruger(username, password);
-            System.out.println("User: "+ user.getBrugernavn()+ " - Sucessfull login");
+            System.out.println("User: " + user.getBrugernavn() + " - Sucessfull login");
             loginSucess = Boolean.TRUE;
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        
-        System.out.println("Bruger: "+ username);
-        
-        
-        if (loginSucess){
+
+        System.out.println("Bruger: " + username);
+
+        if (loginSucess) {
             //check for token
-            
+
             String token = Storage.tokenMap.get(username);
-            
+
             //if no token gennerate one
-            if(token == null){
+            if (token == null) {
                 token = UUID.randomUUID().toString();
                 Storage.tokenMap.put(username, token);
                 System.out.println("Token gennerated: " + token);
             } else {
                 System.out.println("Token already mapped: " + token);
             }
-            
+
             Storage.tokenMap.put(username, token);
             response.put("token", token);
-            
+
             return Response.ok(response.toString(), MediaType.APPLICATION_JSON).build();
         } else {
             response.put("url", "https://http.cat/401");
             return Response.status(Response.Status.UNAUTHORIZED).entity(response.toString()).build();
         }
     }
-    
-    
-    
-    
-    
-    
 
     private static Bruger hentBruger(java.lang.String arg0, java.lang.String arg1) {
         brugerautorisation.transport.soap.BrugeradminImplService service = new brugerautorisation.transport.soap.BrugeradminImplService();
         brugerautorisation.transport.soap.Brugeradmin port = service.getBrugeradminImplPort();
         return port.hentBruger(arg0, arg1);
     }
-    
-    
+
 }
-
-
