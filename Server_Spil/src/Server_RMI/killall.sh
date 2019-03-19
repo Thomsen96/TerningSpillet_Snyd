546e9a839dc97b1e9a2517ef156549a6ec695f05
mthieden <m.thieden@gmail.com>
2019-03-19 12:23:41 +0100

    cleaning of the project

--- Server_Spil/src/Server_RMI/killall.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-kill $(ps aux | grep 'Server.ServerLogik' | awk '{print $2}')
-
--- Server_Spil/src/Server_RMI/killspecific.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#Få fat i porten
-echo 'porten er' \"$1\"
-
-kill $(ps aux | grep 'Server.ServerLogik [0-9]* [0-9] '$1 | awk '{print $2}')
